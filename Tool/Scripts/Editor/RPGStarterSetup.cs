fae5e5ad3f3a8bb6887e7092dd72eb886c01b698
Burmuruk <santoyo.herrera.david@gmail.com>
2025-12-11 01:06:19 -0600

    Copy option without validations

--- Tool/Scripts/Editor/RPGStarterSetup.cs
+++ Tool/Scripts/Editor/RPGStarterSetup.cs
@@ -4,6 +4,15 @@ using System.IO;
 
 namespace Burmuruk.RPGStarterTemplate.Editor
 {
+    public class RPGStarterSetupMenu : EditorWindow
+    {
+        [MenuItem("RPGTemplate/Copy Architecture", priority = 15)]
+        public static void ShowWindow()
+        {
+            RPGStarterSetup.CopyFiles();
+        }
+    }
+
     [InitializeOnLoad]
     public static class RPGStarterSetup
     {
@@ -13,77 +22,25 @@ namespace Burmuruk.RPGStarterTemplate.Editor
 
         static RPGStarterSetup()
         {
-            if (!AssetDatabase.IsValidFolder("Assets/RPG-Results"))
-            {
-                if (!EditorPrefs.HasKey(copyPref) || !EditorPrefs.GetBool(copyPref))
-                    return;
-
-                AssetDatabase.CreateFolder("Assets/", "RPG-Results");
-                AssetDatabase.Refresh();
-            }
-
+            Debug.Log("Initialized");
             if (!Directory.Exists(targetPath))
             {
-                if (!EditorPrefs.HasKey(copyPref) || !EditorPrefs.GetBool(copyPref))
-                    return;
-                using UnityEditor;
-                using UnityEngine;
-                using System.IO;
+                //if (!EditorPrefs.HasKey(copyPref) || !EditorPrefs.GetBool(copyPref))
+                //    return;
 
-namespace Burmuruk.RPGStarterTemplate.Editor
-    {
-        public class RPGStarterSetupMenu : EditorWindow
-        {
-            [MenuItem("RPGTemplate/Copy Architecture")]
-            public static void ShowWindow()
-            {
-                RPGStarterSetup.CopyFiles();
+                EditorPrefs.SetBool(copyPref, CopyFiles());
             }
         }
 
-        [InitializeOnLoad]
-        public static class RPGStarterSetup
+        public static bool CopyFiles()
         {
-            static readonly string sourcePath = "Packages/com.Burmuruk.RPG-Starter-Template/GameArchitecture";
-            static readonly string targetPath = "Assets/GameArchitecture";
-            const string copyPref = "CopyRPGFiles";
-
-            static RPGStarterSetup()
+            if (EditorPrefs.HasKey(copyPref))
             {
-                if (!Directory.Exists(targetPath))
-                {
-                    if (!EditorPrefs.HasKey(copyPref) || !EditorPrefs.GetBool(copyPref))
-                        return;
-
-                    EditorPrefs.SetBool(copyPref, CopyFiles());
-                }
+                EditorPrefs.DeleteKey(copyPref);
+                Debug.Log("RPG Starter Template: Copy preference reset. You can copy the files again from the RPGTemplate menu.");
+                return true;
             }
 
-            public static bool CopyFiles()
-            {
-                if (EditorUtility.DisplayDialog("RPG Starter Template",
-                        "Do you want to copy the base files to Assets/GameArchitecture?",
-                        "yes, copy", "No"))
-                {
-                    FileUtil.CopyFileOrDirectoryFollowSymlinks(sourcePath, targetPath);
-                    AssetDatabase.Refresh();
-                    Debug.Log("RPG Starter Template: GameArchitecture copied to Assets/");
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-        }
-    }
-
-    CopyFiles();
-            }
-        }
-
-        static void CopyFiles()
-        {
             if (EditorUtility.DisplayDialog("RPG Starter Template",
                     "Do you want to copy the base files to Assets/GameArchitecture?",
                     "yes, copy", "No"))
@@ -91,11 +48,11 @@ namespace Burmuruk.RPGStarterTemplate.Editor
                 FileUtil.CopyFileOrDirectoryFollowSymlinks(sourcePath, targetPath);
                 AssetDatabase.Refresh();
                 Debug.Log("RPG Starter Template: GameArchitecture copied to Assets/");
-                EditorPrefs.SetBool(copyPref, true);
+                return true;
             }
             else
             {
-                EditorPrefs.SetBool(copyPref, false);
+                return false;
             }
         }
     }
