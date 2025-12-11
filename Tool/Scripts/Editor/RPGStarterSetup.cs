d3cd4d02dd4d4772f5ab165886e0f149734198a6
Burmuruk <santoyo.herrera.david@gmail.com>
2025-12-11 00:46:01 -0600

    Copy option added

--- Tool/Scripts/Editor/RPGStarterSetup.cs
+++ Tool/Scripts/Editor/RPGStarterSetup.cs
@@ -26,8 +26,59 @@ namespace Burmuruk.RPGStarterTemplate.Editor
             {
                 if (!EditorPrefs.HasKey(copyPref) || !EditorPrefs.GetBool(copyPref))
                     return;
+                using UnityEditor;
+                using UnityEngine;
+                using System.IO;
 
-                CopyFiles();
+namespace Burmuruk.RPGStarterTemplate.Editor
+    {
+        public class RPGStarterSetupMenu : EditorWindow
+        {
+            [MenuItem("RPGTemplate/Copy Architecture")]
+            public static void ShowWindow()
+            {
+                RPGStarterSetup.CopyFiles();
+            }
+        }
+
+        [InitializeOnLoad]
+        public static class RPGStarterSetup
+        {
+            static readonly string sourcePath = "Packages/com.Burmuruk.RPG-Starter-Template/GameArchitecture";
+            static readonly string targetPath = "Assets/GameArchitecture";
+            const string copyPref = "CopyRPGFiles";
+
+            static RPGStarterSetup()
+            {
+                if (!Directory.Exists(targetPath))
+                {
+                    if (!EditorPrefs.HasKey(copyPref) || !EditorPrefs.GetBool(copyPref))
+                        return;
+
+                    EditorPrefs.SetBool(copyPref, CopyFiles());
+                }
+            }
+
+            public static bool CopyFiles()
+            {
+                if (EditorUtility.DisplayDialog("RPG Starter Template",
+                        "Do you want to copy the base files to Assets/GameArchitecture?",
+                        "yes, copy", "No"))
+                {
+                    FileUtil.CopyFileOrDirectoryFollowSymlinks(sourcePath, targetPath);
+                    AssetDatabase.Refresh();
+                    Debug.Log("RPG Starter Template: GameArchitecture copied to Assets/");
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+
+    CopyFiles();
             }
         }
 
